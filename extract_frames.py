13a4ecd84c6d708fc3bde1b0b906d0ad23fb4995
Nizar <nizar4790k@gmail.com>
2021-03-05 23:47:44 +0000

    cheking if rval is not null to avoid that extract_frames.py throws an error and extract_processed was modified to be able to write a images.txt file that include all the images path included in processed_images2

--- extract_frames.py
+++ extract_frames.py
@@ -6,16 +6,48 @@ import tqdm
 from pathlib import Path
 from natsort import natsorted
 import numpy as np
-# video_name = sys.argv[1]
-root = "../Data/test-data/"
+#video_name = sys.argv[1]
+
+
+
+
+# file_path is a path to the directory that the file containing the names is in
+# file_name is the name of the text file
+# returns a numpy array of strings of the names of the video files
+def read_video_file_names(file_path, file_name) -> np.ndarray:
+    '''
+    Reads in the names of the video files when given the name of the text file
+    containing the names and the path to it
+    '''
+    id_file = open(file_path + '/' + id_file_name)
+    video_file_names = []
+
+    # loop through file and create list of the file names
+    for line in id_file:
+        line = line.strip() # remove trailing whitespace
+        temp, video_file_name = line.split(' ') # split the line at a space
+        video_file_names.append(video_file_name) # append to the list of file names
+
+    id_file.close() # close the text file
+
+    # convert list to numpy array for convenience
+    return np.array(video_file_names)
+
+
+
+root = "/content/gdrive/Shared drives/UASD Fondocyt Proyecto 911/Datasets/AI city challenge/AIC20_track1/Dataset_A/"
+id_file_name = 'list_video_id.txt'
 dest_dir = "ori_images/"
-video_names = [str(i)+'.mp4' for i in range(1,101)]
+video_names=["cam_1.mp4"]
+#video_names = read_video_file_names(root, root+'/'+id_file_name) # read all videos
 print("caputure videos")
 for video_name in tqdm.tqdm(video_names):
     file_name = video_name
     folder_name = dest_dir+file_name.split('.')[0]
     os.makedirs(folder_name,exist_ok=True)
-    vc = cv2.VideoCapture(root+video_name)
+   
+    vc = cv2.VideoCapture(root+'/'+video_name)
+    
     c = 1
     if vc.isOpened():
         rval, frame = vc.read()
@@ -27,7 +59,10 @@ for video_name in tqdm.tqdm(video_names):
     while rval: 
         rval, frame = vc.read()
         pic_path = folder_name+'/'
-        if (c % timeF == 0): 
+        
+       
+        if (c % timeF == 0 and rval): 
+            print(pic_path + str(c) + '.jpg')
             cv2.imwrite(pic_path + str(c) + '.jpg', frame) 
         c = c + 1
         cv2.waitKey(1)
@@ -38,12 +73,13 @@ dest_dir_processed = "processed_images/"
 if not os.path.isdir(dest_dir_processed):
     os.mkdir(dest_dir_processed)
 print("average images")
-for i in tqdm.tqdm(range(1,101)):
-    video_name = str(i)
+
+for vn in video_names:
+    video_name = vn.split('.')[0]
     path_file_number=glob.glob(os.path.join(dest_dir,video_name,'*.jpg')) 
     internal_frame = 4
     start_frame = 100
-    video_name = str(i)
+   
     nums_frames = len(path_file_number)
     alpha=0.1
     Path(dest_dir_processed+video_name).mkdir(exist_ok=True)
@@ -51,12 +87,13 @@ for i in tqdm.tqdm(range(1,101)):
     for j in range(4,5):
         internal_frame = 100
         num_pic = int(nums_frames)
-        former_im = cv2.imread(dest_dir+"%d/100.jpg"%i)
+        former_im = cv2.imread(dest_dir+video_name+"/100.jpg")
         img = cv2.imread(os.path.join(dest_dir,video_name,str(start_frame)+'.jpg'))
         for i in range(num_pic):
             # print(os.path.join(dest_dir,video_name,str(start_frame)+'.jpg'))
             # print(os.path.join(dest_dir,video_name,str(i*internal_frame+start_frame)+'.jpg'))
             now_im = cv2.imread(os.path.join(dest_dir,video_name,str(i*internal_frame+start_frame)+'.jpg'))
+            print(num_pic)
             if np.mean(np.abs(now_im-former_im))>5:
                 img = img*(1-alpha)+now_im*alpha
                 cv2.imwrite(dest_dir_processed+video_name+'/'+str(i*internal_frame+start_frame)
@@ -65,3 +102,7 @@ for i in tqdm.tqdm(range(1,101)):
                 cv2.imwrite(dest_dir_processed+video_name+'/'+str(i*internal_frame+start_frame)
                         +'_'+str(j)+'.jpg',img*0)
             former_im = now_im
+
+
+
+
--- extract_processed.py
+++ extract_processed.py
@@ -8,14 +8,22 @@ base2 = "processed_images2/"
 
 folders = natsorted(os.listdir(base))
 
+image_text_file=open("images.txt","w")
+  
 for fo in folders:
 	files = natsorted(os.listdir(base+fo+'/'))
 	if not os.path.isdir(base2+fo):
 		os.mkdir(base2+fo)
 
 	for f in files:
+		images_path=[str(base2+fo+ "/" +f.split('.')[0] + "_1" + ".jpg"),
+		str(base2+fo+ "/" +f.split('.')[0] + "_2" + ".jpg"),
+		str(base2+fo+ "/" +f.split('.')[0] + "_3" + ".jpg")]
 		D = cv2.imread(base+fo+'/'+f)
-		cv2.imwrite(base2+fo+ "/" +f.split('.')[0] + "_1" + ".jpg",D[:,0:400,:])
-		cv2.imwrite(base2+fo+ "/" +f.split('.')[0] + "_2" + ".jpg",D[:,200:600,:])
-		cv2.imwrite(base2+fo+ "/" +f.split('.')[0] + "_3" + ".jpg",D[:,400:800,:])
+		cv2.imwrite(images_path[0],D[:,0:400,:])
+		cv2.imwrite(images_path[1],D[:,200:600,:])
+		cv2.imwrite(images_path[2],D[:,400:800,:])
 
+		for j in range(0,len(images_path)):
+			image_text_file.write('/content/gdrive/My Drive/Colab Notebooks/NVIDIA_AICITY/'+images_path[j])
+			image_text_file.write('\n')
